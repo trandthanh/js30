5ead16ff61afa4e7c8905126c1450dede87d7907
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-07-25 17:11:10 +0200

    Finished Challenge 22

--- app/javascript/challenges/challenge22.js
+++ app/javascript/challenges/challenge22.js
@@ -1,5 +1,27 @@
 function challenge22() {
 
+  const triggers = document.querySelectorAll('a');
+  const highlight = document.createElement('span');
+  highlight.classList.add('highlight');
+  document.getElementById('challenge22').append(highlight);
+
+  function highlightLink() {
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    const coords = {
+      width: linkCoords.width,
+      height: linkCoords.height,
+      top: linkCoords.top + window.scrollY,
+      left: linkCoords.left + window.scrollX
+    };
+
+    highlight.style.width = `${linkCoords.width}px`;
+    highlight.style.height = `${linkCoords.height}px`;
+    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+  }
+
+  triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+
 }
 
 export { challenge22 };
